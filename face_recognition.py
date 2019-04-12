59017d018231562b3595824ba9ea714b470b2be6
Nicola Shi <nicola.shi@tufts.edu>
2019-04-12 15:58:32 +0800

    clean up data

--- face_recognition.py
+++ face_recognition.py
@@ -11,34 +11,34 @@ import base64
 
 def main():
 
-    directory = "data/"
+    directory = "./face_recognition_data/test/"
 
-    api_url = "https://aip.baidubce.com/rest/2.0/face/v2/identify"
+    api_url = "https://aip.baidubce.com/rest/2.0/face/v3/search"
     access_token = '24.9e4fc89b43b955ff1d803c189446a47c.2592000.1557630178.282335-15993344'
     request_url = api_url + "?access_token=" + access_token
 
-
     # traverse all test image files and output results for each image
-    for root, dirs, files in os.walk(directory):
+    for root, dirs, files in os.walk(directory, topdown=False):
         for filename in files:
             if filename.endswith(".jpg"):
-                print(filename)
                 filepath = root + "/" + filename
+		print(filepath)
                 curr_img = extract_img(filepath)
-                #print("\n" + filepath + "\n")
-           
-                #params = "{\"image\":\"" + str(curr_img) + \
-                #"027d8308a2ec665acb1bdf63e513bcb9\"," +\
-                #"\"image_type\":\"BASE64\"," +\
-                #"\"max_face_num\":10 ," +\
-                #"\"face_field\":\"" + fields + "\"}"
+		
+		#print(str(curr_img))	
+
+                params = "{\"image\":\"" + str(curr_img) + "\"," +\
+		"\"image_type\":\"BASE64\"," +\
+                "\"group_id_list\":\"\"comparison_test\"}"
             
-                #request = urllib2.Request(url=request_url, data=params)
-                #request.add_header('Content-Type', 'application/json')
-                #response = urllib2.urlopen(request)
-                #content = response.read()
-                #if content:
-                    #print(content)
+		#print(params)
+
+                request = urllib2.Request(url=request_url, data=params)
+                request.add_header('Content-Type', 'application/json')
+                response = urllib2.urlopen(request)
+                content = response.read()
+                if content:
+                    print(content)
 
 
 #
